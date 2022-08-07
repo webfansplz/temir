a677a42b55f561c7ec65e29a8b68b9edd1ada429
webfansplz <308241863@qq.com>
2022-08-07 12:04:30 +0800

    feat: add example

---
+++ examples/borders/src/App.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { TBox, TText } from '@temir/core'
+</script>
+
+<template>
+  <TBox flex-direction="column" :padding="2">
+    <TBox>
+      <TBox border-style="single" :margin-right="2">
+        <TText>single</TText>
+      </TBox>
+
+      <TBox border-style="double" :margin-right="2">
+        <TText>double</TText>
+      </TBox>
+
+      <TBox border-style="round" :margin-right="2">
+        <TText>round</TText>
+      </TBox>
+
+      <TBox border-style="bold">
+        <TText>bold</TText>
+      </TBox>
+    </TBox>
+
+    <TBox :margin-top="1">
+      <TBox border-style="singleDouble" :margin-right="2">
+        <TText>singleDouble</TText>
+      </TBox>
+
+      <TBox border-style="doubleSingle" :margin-right="2">
+        <TText>doubleSingle</TText>
+      </TBox>
+
+      <TBox border-style="classic">
+        <TText>classic</TText>
+      </TBox>
+    </TBox>
+  </TBox>
+</template>
--- examples/hi-temir/src/App.vue
+++ examples/hi-temir/src/App.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup>
-import { TText } from '@temir/core'
+import { ref } from '@vue/runtime-core'
+import { TBox, TText } from '@temir/core'
+const count = ref(0)
+setInterval(() => {
+  count.value++
+}, 100)
 </script>
 
 <template>
-  <TText color="#42b883">
-    🌈 Hi TEMIR !
-  </TText>
+  <TBox
+    :margin="5"
+    :width="30"
+    :height="5"
+    border-style="double"
+    justify-content="center"
+    align-items="center"
+  >
+    <TText color="#42b883">
+      🌈 Hi TEMIR !! {{ count }}
+    </TText>
+  </TBox>
 </template>
