64b4a53dfd6f17b1e5fd9930ef2b1945b4eccc73
webfansplz <308241863@qq.com>
2022-08-08 00:18:07 +0800

    feat: add example

---
+++ examples/temir-select-input/src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import TSelectInput from '@temir/select-input'
+
+const items = [
+  {
+    label: 'Vue',
+    value: 'Vue',
+  },
+  {
+    label: 'Vite',
+    value: 'Vite',
+  },
+  {
+    label: 'Temir',
+    value: 'Temir',
+  },
+]
+function onSelect(value) {
+  console.log('selected', value)
+}
+</script>
+
+<template>
+  <TSelectInput :items="items" :on-select="onSelect" />
+</template>
