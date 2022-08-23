a7d544458bd3e51480da5ec353f60a21db2d58db
vaakian <vaakian@gmail.com>
2022-08-23 22:53:44 +0800

    fix(staticOutput): initialize `fullStaticOutput`

--- packages/temir/src/temir.ts
+++ packages/temir/src/temir.ts
@@ -34,7 +34,7 @@ export default class Temir {
   // Ignore last render after unmounting a tree to prevent empty output before exit
   private isUnmounted: boolean
   private rootNode: dom.DOMElement
-  private fullStaticOutput: string
+  private fullStaticOutput = ''
   private lastOutput: string
   private exitPromise?: Promise<void>
   private restoreConsole?: () => void
